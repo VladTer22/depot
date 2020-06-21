75492d47337ec7480731b003e5814187fcb263ce
VladTer22 <volodymyrtereshchuk22@gmail.com>
2020-06-21 18:00:45 +0300

    Logging

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   include CurrentCart
+  skip_before_action :authorize, only: [:new, :create]
   before_action :set_cart, only: [:new, :create]
   before_action :ensure_cart_isnt_empty, only: :new
   before_action :set_order, only: [:show, :edit, :update, :destroy]
@@ -34,6 +35,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        ChargeOrderJob.perform_later(@order,pay_type_params.to_h)
         format.html { redirect_to store_index_url, notice:
             'Thank you for your order.' }
         format.json { render :show, status: :created,
@@ -80,10 +82,26 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:name, :address, :email, :pay_type)
   end
+  #...
 
+  private
   def ensure_cart_isnt_empty
     if @cart.line_items.empty?
       redirect_to store_index_url, notice: 'Your cart is empty'
     end
   end
+
+
+  def pay_type_params
+    if order_params[:pay_type] == "Credit card"
+      params.require(:order).permit(:credit_card_number, :expiration_date)
+    elsif order_params[:pay_type] == "Check"
+      params.require(:order).permit(:routing_number, :account_number)
+    elsif order_params[:pay_type] == "Purchase order"
+      params.require(:order).permit(:po_number)
+    else
+      {}
+    end
+  end
+
 end
